e6869ceb76538d648816db8d8134b3b791b13ef6
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2023-12-06 19:52:33 -0300

    Inicialização do Projeto

---
+++ src/main/java/br/edu/infnet/criadordepersonagem/model/testes/SalvaguardasTeste.java
@@ -0,0 +1,12 @@
+package br.edu.infnet.criadordepersonagem.model.testes;
+
+public class SalvaguardasTeste {
+    public static void main(String[] args) {
+        System.out.println("SALVAGUARDAS - TESTE\n");
+        System.out.println("""
+                Instruções:
+                Após a definição dos atributos e seus modificadores, podemos utilizar esses modificadores em testes específicos.
+                """);
+
+    }
+}
